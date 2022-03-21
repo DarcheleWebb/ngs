4b45b9e9f2f769aa1306527088046f63c5f81691
Darchele Webb <Darchele.Webb@rgare.com>
2022-03-21 11:20:51 -0500

    updated gitignore and wrapper example

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,7 +1,14 @@
 // function (exports, module, require, __filename, __dirname) {
 
-let g = 1; //not global -- scoped to wrapping function
+// let g = 1; //not global -- scoped to wrapping function
 
-console.log(arguments);
+// console.log(arguments);
 
+exports.a = 42;
+module.exports.b = 37;
+
+// exports = () => {}; //not ok
+// module.exports = () => {}; //ok
+
+// return module.exports;S
 // }
